ba59256d6c4b51c4b821d8f74548531596fe1fa9
Daniel <mocaibre@gmail.com>
2025-08-06 17:35:47 -0300

    estilizando footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,25 +9,27 @@ function Footer() {
 
   return (
     <>
-      <div className="flex justify-center bg-blue-400 text-white">
-        <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">
-            Blog Pessoal Generation | Copyright: {data}
-          </p>
-          <p className="text-lg">Acesse nossas redes sociais</p>
-          <div className="flex gap-2">
-            <a
-              href="https://www.linkedin.com/in/daniel-castro-de-souza-602491133/"
-              target="_blank"
-            >
-              <LinkedinLogo size={48} weight="bold" />
-            </a>
-            <a href="https://www.instagram.com/seu_usuario" target="_blank">
-              <InstagramLogo size={48} weight="bold" />
-            </a>
-            <a href="https://www.facebook.com/seu_usuario" target="_blank">
-              <FacebookLogo size={48} weight="bold" />
-            </a>
+      <div className="flex justify-center">
+        <div className="flex justify-center bg-blue-400 text-white w-1/2 rounded-2xl m-1">
+          <div className="container flex flex-col items-center py-4">
+            <p className="text-xl font-bold">
+              Blog Pessoal Generation | Copyright: {data}
+            </p>
+            <p className="text-lg">Acesse nossas redes sociais</p>
+            <div className="flex gap-2">
+              <a
+                href="https://www.linkedin.com/in/daniel-castro-de-souza-602491133/"
+                target="_blank"
+              >
+                <LinkedinLogo size={48} weight="bold" />
+              </a>
+              <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                <InstagramLogo size={48} weight="bold" />
+              </a>
+              <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                <FacebookLogo size={48} weight="bold" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
